96a91d6b6d1a5f1357754048b5b382daeeab896a
flerpo <alex.rasten@gmail.com>
2024-05-19 22:23:38 +0200

    New and improved?

--- scripts/setupwsl.sh
+++ scripts/setupwsl.sh
@@ -13,19 +13,63 @@ case $choice in
     sudo apt install wget curl git zsh -y
     echo "Setting zsh as default shell (password prompt may appear)"
     chsh -s $(which zsh)
-    sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-    ;;
-
-  2)
-    echo "Making stuff pretty..."
     mkdir -p ~/Downloads/fonts
     cd ~/Downloads/fonts
     wget https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Regular.ttf
     wget https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold.ttf
     wget https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Italic.ttf
     wget https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold%20Italic.ttf
+    
+    ;;
 
-    sudo git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/.oh-my-zsh/custom/themes/powerlevel10k
+  2)
+    echo "Making stuff pretty..."
+
+
+    mv ~/.zshrc ~/.zshrc.bak
+    touch ~/.zshrc
+
+    {
+    echo "ZINIT_HOME='${XDH_DATA_HOME:-${HOME}/.local/share}/zinit/zinit.git'"
+    echo "if [ ! -d \"\$ZINIT_HOME\" ]; then"
+    echo "mkdir -p \"\$(dirname \$ZINIT_HOME)\""
+    echo "git clone https://github.com/zdharma-continuum/zinit.git '$ZINIT_HOME'"
+    echo "fi"
+
+    echo "source \${ZINIT_HOME}/zinit.zsh "
+    echo "zinit ice depth=1; zinit light romkatv/powerlevel10k"
+
+    echo "zinit light zsh-users/zsh-syntax-highlighting"
+    echo "zinit light zsh-users/zsh-completions"
+    echo "zinit light zsh-users/zsh-autosuggestions"
+    echo "zinit snippet OMZP::git"
+    echo "zinit snippet OMZP::sudo"
+    echo "zinit snippet OMZP::kubectl"
+    echo "zinit snippet OMZP::kubectx"
+    echo "zinit snippet OMZP::command-not-found"
+
+
+    echo "autoload -U compinit && compinit"
+
+    echo "zinit cdreplay -q"
+
+    echo "HISTSIZE=5000" 
+    echo "HISTFILE=~/.zsh_history" 
+    echo "SAVEHIST=$HISTSIZE" 
+    echo "HISTDUP=erase" 
+    echo "setopt appendhistory" 
+    echo "setopt sharehistory" 
+    echo "setopt hist_ignore_space" 
+    echo "setopt hist_ignore_all_dups" 
+    echo "setopt hist_save_no_dups" 
+    echo "setopt hist_ignore_dups" 
+    echo "setopt hist_find_no_dups"
+
+    
+    echo "alias ls=ls --color"
+
+    } >> ~/.zshrc
+    
     sed -i 's/robbyrussell/powerlevel10k\/powerlevel10k/g' ~/.zshrc
     echo "Remember to change the font for Ubuntu terminal!"
     echo "Restart the terminal window!"
@@ -41,18 +85,12 @@ case $choice in
     curl -LO "https://dl.k8s.io/release/$kubectl_url/bin/linux/amd64/kubectl"
     chmod +x kubectl
     sudo mv kubectl /usr/local/bin
-
-    mkdir -p ~/.oh-my-zsh/completions
-    chmod -R 755 ~/.oh-my-zsh/completions
-    ln -sf /opt/kubectx/completion/kubectx.zsh ~/.oh-my-zsh/completions/_kubectx.zsh
-    ln -sf /opt/kubectx/completion/kubens.zsh ~/.oh-my-zsh/completions/_kubens.zsh
+    
     ;;  
   4)
     {
-    echo "alias k=kubectl"
     echo "alias kn=kubens"
     echo "alias switch=kubectx"
-    echo "alias g=git"
     } >> ~/.zshrc
     echo "Aliases set. Restart the terminal or run 'source ~/.zshrc'"
     ;;

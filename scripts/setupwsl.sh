951eb4b13a427dd94124120cac16cdf4792eeb29
Alex Råsten <alex.rasten@gmail.com>
2024-08-22 09:21:35 +0200

    removed blankspace

--- scripts/setupwsl.sh
+++ scripts/setupwsl.sh
@@ -20,7 +20,6 @@ case $choice in
     wget https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold.ttf
     wget https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Italic.ttf
     wget https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold%20Italic.ttf
-    
     ;;
 
   2)
